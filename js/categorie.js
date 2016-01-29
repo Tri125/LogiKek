c10445f93ccdd4980532eb92fce7b1ad218901d4
Tristan <kraig.tri125@gmail.com>
2016-01-28 23:36:29 -0500

    Example fonctionnelle de Angular.js

--- js/categorie.js
+++ js/categorie.js
@@ -1,35 +1,13 @@
 
-var magasin = angular.module('magasin', ['ui.router','ui.bootstrap']);
+var magasin = angular.module('magasin', []);
 
-	magasin.controller('CategorieListController', function() {
-		var categorieList = this;
-		categorieList.categories = [
-		{nom:'learn angular'},
-		{nom:'build an angular app'}];
+	magasin.controller('CategorieListController', function($scope) {
+		$scope.categories = categories;
 
 });
 
 
-magasin.controller('DropdownCtrl', function ($scope, $log) {
-  $scope.items = [
-    'The first choice!',
-    'And another choice for you.',
-    'but wait! A third!'
-  ];
-
-  $scope.status = {
-    isopen: false
-  };
-
-  $scope.toggled = function(open) {
-    $log.log('Dropdown is now: ', open);
-  };
-
-  $scope.toggleDropdown = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    $scope.status.isopen = !$scope.status.isopen;
-  };
-
-  $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
-});
+var categories = [
+		{nom:'learn aaaa'},
+		{nom:'build an angular app'}
+		];
