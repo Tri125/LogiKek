abe536f13da7af257cb357fff4696e5d00bb5c73
Tristan <kraig.tri125@gmail.com>
2016-02-09 20:23:17 -0500

    Nouveau div pour contenir la navigation par catégories. Empêche le shadow sur une div vide puisque id était en dehors de la condition php.

--- sectionGauche.php
+++ sectionGauche.php
@@ -1,12 +1,14 @@
 <div class="row"> <!-- Début ligne du contenu central -->
-	<div id="rechercheCat" class="col-md-2"> <!-- Section pour parcourir les produits selon leurs catégories -->
+	<div class="col-md-2"> <!-- Section pour parcourir les produits selon leurs catégories -->
 		<?php if($estIndex) : //Cache la navigation par catégories si nous ne sommes pas à la page index.php ?>
-		<h3>Naviguer par catégories</h3>
-		<ul>
-			<li><a href="./">Toutes</a></li>
-		<?php foreach(Categorie::fetchAll() as $value): ?>
-			<li><a href="./?listeCategorie=<?php echo $value->getCodeCategorie(); ?>"><?php echo $value->getNom(); ?></a></li>
-		<?php endforeach; ?>
-		</ul>
+		<div id="rechercheCat">
+			<h3>Naviguer par catégories</h3>
+			<ul>
+				<li><a href="./">Toutes</a></li>
+			<?php foreach(Categorie::fetchAll() as $value): ?>
+				<li><a href="./?listeCategorie=<?php echo $value->getCodeCategorie(); ?>"><?php echo $value->getNom(); ?></a></li>
+			<?php endforeach; ?>
+			</ul>
+		</div>
 		<?php endif; ?>
 	</div> <!-- Fin section pour parcourir selon les catégories -->
