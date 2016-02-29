eb47a7da92eee6d9304c2fa41f29e72f5f4406e6
Tristan <kraig.tri125@gmail.com>
2016-02-29 14:14:32 -0500

    Mise à jour dossier et affichage d'un message lorsqu'aucune mise à jour à été faite.

--- creationDossier.php
+++ creationDossier.php
@@ -12,27 +12,50 @@ $motCle = 'OS, Linux, Windows, BSD, Apple, RHEL, Vente, logiciel';
 
 
 global $maBD;
+$message = '';
 
 if (isset($_SESSION['client']))
-{
+{	
 	$client = $_SESSION['client'];
-	try
+	
+	if (isset($_SESSION['authentification']))
 	{
- 		$maBD->insertClient($client);
-	}
-	catch(Exception $e)
-	{
-		//Doublon
-		if ($e->getMessage() == 1062)
+		$nbrLigneJour;
+		
+		try
 		{
-			header("location:inscription.php?erreur=doublon");
+			$nbrLigneJour =	$maBD->updateClient($client);
+		}
+		catch (Exception $e)
+		{
 			exit();
 		}
-		else
-			exit();
+		
+		if ($nbrLigneJour == 0)
+			$message = 'Vous n\'avez fait aucun changement';
+		$_SESSION['modif'] = $nbrLigneJour;
 	}
-	$_SESSION['authentification'] = $client->getNomUtilisateur();
+	else
+	{
+		try
+		{
+ 			$maBD->insertClient($client);
+		}
+		catch (Exception $e)
+		{
+			//Doublon
+			if ($e->getMessage() == 1062)
+			{
+				header("location:inscription.php?erreur=doublon");
+				exit();
+			}
+			else
+				exit();
+		}
+		$_SESSION['authentification'] = $client->getNomUtilisateur();
+	}
 }
+//Usager à lui même été sur la page sans créer un objet client par la page inscription.php
 else
 {
 	header("location:inscription.php");
@@ -49,19 +72,26 @@ require_once("./sectionGauche.php");
 	<!-- Début des produits -->
 	<div class="row">
 		<div class="container">
-		<?php if (isset($_SESSION['authentification'])): ?>
+		<?php if (isset($_SESSION['authentification']) && isset($_SESSION['modif'])): ?>
+			<?php if(!empty($message)): ?>
+				<div class="alert alert-warning" role="alert"> <!-- Message indiquant un panier vide. -->
+					<i class="fa fa-info-circle"></i>
+					<?php echo $message;?>
+				</div>
+			<?php endif; ?>
+			<h3><?php echo $client->getPrenom().' '.$client->getNom(); ?></h3>
+  			<p>Votre dossier a été mit à jour avec succès.</p>
+		<?php elseif (isset($_SESSION['authentification'])): ?>
 			<h3><?php echo $client->getPrenom().' '.$client->getNom(); ?></h3>
   			<p>Votre dossier a été créé avec succès, vous pouvez dorénavant commander des produits.</p>
   			<p>À votre prochain visite, il vous suffira de vous authentifier avec votre nom d'usager: <?php echo $client->getNomUtilisateur(); ?> et votre mot de passe. </p>
   			<p>Pour cette session, vous êtes déjà authentifiée.</p>
   			<p>Cliquez pour retourner au <a href="./" >catalogue</a></p>
+
   		<?php else: ?>
-  			<h3>Une erreur est survenue.</h3>
-  			<p>Veuillez créer votre dossier à partir de la page d'inscription.</p>
+		  <h3>Une erreur est survennue</h3>
   		<?php endif; ?>
   		</div>
-
-
 <!-- Contenu principal -->
 
 
