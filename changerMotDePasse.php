9a57b0ccb7ed958ea4f71f6f2a00e80b6fc1ec7b
Tristan <kraig.tri125@gmail.com>
2016-03-01 14:26:17 -0500

    Fix HTML balise fermante manquante

--- changerMotDePasse.php
+++ changerMotDePasse.php
@@ -168,21 +168,19 @@ else
 <!-- Modal -->
 <div id="modalErreur" class="modal fade" role="dialog">
   <div class="modal-dialog">
-
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">Erreur</h4>
       </div>
-      <div class="modal-body">
-      </div>
+      <div class="modal-body"></div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
       </div>
     </div>
-
   </div>
+</div>
 
 
 <!-- Contenu principal -->
