0ab110e40f88bd9853804fb0e5d77db60f1313c5
Tristan <kraig.tri125@gmail.com>
2016-03-22 16:13:51 -0400

    Commentaire

--- js/rafraichissement.js
+++ js/rafraichissement.js
@@ -2,6 +2,10 @@
 // Script pour rafraichir la page automatiquement à chaque 15 secondes
 //------------------------------
 
+
+//------------------------------
+// Appel une fonction après un certain temps
+//------------------------------
 function Chrono()
 {
 	setTimeout(function(){
@@ -9,12 +13,17 @@ function Chrono()
 	}, 15000);
 }
 
+//------------------------------
+// Rafraichis la page
+//------------------------------
 function rafraichis() {
 	var url = './historiqueCommande.php';
 	window.location.href = url;
 }
 
-
+//------------------------------
+// Une fois le DOM chargé, exécute la fonction
+//------------------------------
 $( document ).ready(function() {
     Chrono();
 });
