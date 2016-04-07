f89bc110b920c85cb73b847e87ae50f530bc59d9
Tristan <kraig.tri125@gmail.com>
2016-04-07 16:32:58 -0400

    Commentaires

--- admin/gestionProduits.php
+++ admin/gestionProduits.php
@@ -83,7 +83,6 @@ catch (Exception $e)
 //Fonction qui crée un tableau avec comme clée les nom des colonnes de nomChamps passé en paramètre.
 //Retourne un tableau.
 //Utiliser pour l'affichage de message d'erreur.
-//Dynamique 
 //-----------------------------
 function genereMessages($nomChamps)
 {
@@ -103,24 +102,36 @@ function genereMessages($nomChamps)
 	return $messagesErreur;
 }
 
+
+//-----------------------------
+//Fonction qui crée un formulaire html5 de façon dynamique selon les caractéristiques de la table
+//que nous voulons crée le formulaire pour.
+//$nomChamps est les caractéristiques de la table en BD, $produit est un produit spécifique, null si en mode création.
+//-----------------------------
 function genereForm($nomChamps, $produit)
 {
 	global $messagesErreur;
 
+	//Pour chaque colonne de la table
 	foreach ($nomChamps as $key => $value) 
 	{
+		//Récupère le nom de la colonne.
 		$nomCol = $value['COLUMN_NAME'];
-		//Si c'est une colonne de clé primaire, nous ne voulons pas afficher le champs.
+		//Affiche un label de la colonne s'il n'est pas une clée primaire.
 		if (isset($value['COLUMN_KEY']) && $value['COLUMN_KEY'] != 'PRI')
 		{
+			//Message d'erreur de validation
 			echo "<span class='erreur'>".$messagesErreur[$nomCol]."</span>";
 			echo "<br>";
+			//Nom du champs avec première lettre en majuscule.
 			echo "<label>".ucfirst($nomCol).": </label>";
 		}
-
-		if (isset($value['COLUMN_KEY']) && $value['COLUMN_KEY'] == 'PRI')
+		//Si c'est une colonne de clée primaire.
+		elseif(isset($value['COLUMN_KEY']) && $value['COLUMN_KEY'] == 'PRI')
 		{
+			//input hidden de la clée.
 			echo "<input type='hidden' name='".$nomCol."' value='";
+			//Si on modifie un produit, met comme value l'id, sinon -1 pour indiquer un nouveau produit (clée non existante).
 			if(isset($produit[$nomCol]))
 				echo $produit[$nomCol];
 			else
@@ -128,38 +139,44 @@ function genereForm($nomChamps, $produit)
 			echo "'>";
 			continue;
 		}
-		elseif(isset($value['COLUMN_KEY']) && $value['COLUMN_KEY'] == 'PRI')
-			continue;
-
+		//Si la colonne à un maximum de caractère et qu'il est plus grand que 60.
 		if (isset($value['CHARACTER_MAXIMUM_LENGTH']) && $value['CHARACTER_MAXIMUM_LENGTH'] > 60)
 		{
+			//Génère un textarea pour écrire le texte.
 			echo "<br>";
 			echo "<textarea rows='4' cols='50' name='".$value['COLUMN_NAME']."'>";
+			//Si un produit est set, met son contenu dans le textarea.
 			if(isset($produit))
 			{
 				echo $produit[$nomCol];
 			}
+			//Sinon, utilise la valeur par défault de la colonne.
 			elseif(isset($value['COLUMN_DEFAULT']))
 			{
 				echo $value['COLUMN_DEFAULT'];
 			}
 			echo "</textarea>";
 		}
+		//Pas un long texte ni une clé primaire.
 		else
 		{
+			//Génère un input de type text.
 			echo "<input type='text' name='".$nomCol."'";
-
+			//Si le produit est set.
 			if(isset($produit))
 			{
+				//Détermine la longueur du texte contenu dans l'objet produit pour ce champ
+				//$size est minimalement 10.
 				if (strlen($produit[$nomCol]) < 10)
 					$size = 10;
 				else
 					$size = strlen($produit[$nomCol]);
-
+				//Valeur et taille de l'input.
 				echo " value='".$produit[$nomCol]."'";
 				echo " size='".$size."'";
 
 			}
+			//Sinon, utilise la valeur par défault de la colonne.
 			elseif(isset($value['COLUMN_DEFAULT']))
 			{
 				echo " value='".$value['COLUMN_DEFAULT']."'";
@@ -169,109 +186,164 @@ function genereForm($nomChamps, $produit)
 		echo "<br>";
 		echo "<br>";
 	}
+	//Section hardcoded pour les champs additionnels requis tel que le téléchargement d'image
+	//et la sélection de catégories.
 
+	//Message d'erreur de validation pour la sélection de catégories.
 	echo "<span class='erreur'>".$messagesErreur['categories']."</span>";
 	echo "<br>";
 	echo "<label>Catégories: </label>";
 	echo "<br>";
+	//Génère les checkbox pour le choix de catégories.
 	genereCheckBoxCategorie($produit);
 	echo "<hr>";
-
+	//Limitation de la taille des fichiers.
 	echo "<input type='hidden' name='MAX_FILE_SIZE' value='100000'>";
+	//Message d'erreur de validation pour la petite photo.
 	echo "<span class='erreur'>".$messagesErreur['petitePhoto']."</span>";
 	echo "<br>";
 	echo "<label>Petite photo: </label>";
+	//input de fichier.
 	echo "<input type='file' name='petitePhoto'>";
 	echo "<br>";
+	//Message d'erreur de validation pour la grande photo.
 	echo "<span class='erreur'>".$messagesErreur['grandePhoto']."</span>";
 	echo "<br>";
 	echo "<label>Grande photo: </label>";
+	//input de fichier.
 	echo "<input type='file' name='grandePhoto'>";
 	echo "<br>";
-
+	//submit pour valider et annuler.
+	//Annuler retourne vers le menu.
 	echo "<input type='submit' name='valider' value='Valider'>";
 	echo "<input type='submit' name='annuler' value='Annuler'>";
 }
 
+
+//-----------------------------
+//Fonction qui crée des input de checkbox pour la sélection de catégorie d'un produit.
+//Si un tableau représentant les données d'un produit est passé en paramètre, les catégories du produit seront sélectionnées.
+//-----------------------------
 function genereCheckBoxCategorie($produit = null)
 {
+	//Récupère un tableau d'objet Catégories de toutes les Catégories en BD.
 	$categories = Categorie::fetchAll();
+	//Si un produit est passé en paramètre.
 	if(isset($produit))
+		//Lorsqu'on récupère un produit en BD, les catégories sont présentes dans une colonne et sont séparé par une virgule.
+		//Explode le champ categories pour avoir un tableau de toutes les catégories du produit (sans les virgules).
 		$tabCategorieSelected = explode(",", $produit['categories']);
+	//Pour chaque Catégories en BD.
 	foreach ($categories as $key => $value) 
 	{
+		//À chaque 5 catégories, change de ligne.
 		if ($key % 5 == 0)
 			echo "<br>";
+		//Input de type checkbox. Value est le nom de la catégorie, car nos requête utilise le nom plutôt que l'id.
+		//Noté '[]' pour le nom. De cette manière le POST sera un tableau de toute les sélections.
 		echo "<input type='checkbox' name='categories[]' value='".$value->getNom()."'";
+		//Si produit est set et que la catégorie actuel est contenu dans les catégorie du produit.
 		if (isset($produit) && in_array($value->getNom(), $tabCategorieSelected))
+			//On la sélectionne.
 			echo " checked";
 		echo ">".$value->getNom();
 	}
 }
 
+
+//-----------------------------
+//Fonction qui valide dynamiquement un tableau de donné $data, selon les caractéristiques
+//d'une table en BD passé en paramètre ($nomChmaps).
+//
+//A certaines limitation.
+//-----------------------------
 function valideForm($nomChamps, $data)
 {
 	global $messagesErreur;
-
+	//Flag de validation. Assume valide au départ.
 	$estValide = true;
 
-
+	//S'il n'y a pas de clée categories dans les données, alors il n'y a aucune catégorie de sélectionner.
 	if (!isset($data['categories']))
 	{
+		//Message d'erreur de validation pour le champ catégories du formulaire.
 		$messagesErreur['categories'] = "Veuillez choisir au moins une catégorie.";
 		$estValide = false;
 	}
 
+	//Pour chacun des colonnes de la table en BD.
 	foreach ($nomChamps as $key => $value) 
 	{
+		//Récupère le nom de la colonne.
 		$colName = $value['COLUMN_NAME'];
-		//Si c'est une colonne de clé primaire, nous ne voulons pas afficher le champs.
+
+		//Si c'est une colonne de clé primaire, il n'y a rien à valider.
+		//Techniquement l'utilisateur pourrait modifier l'html du formulaire et briser cette assumption.
 		if (isset($value['COLUMN_KEY']) && $value['COLUMN_KEY'] == 'PRI')
 			continue;
-
+		//Récupère la donnée à valider pour cette colonne.
 		$input = $data[$colName];
 
+		//Si la colonne à une longueur de caractères maximales (généralement un champ texte).
 		if (isset($value['CHARACTER_MAXIMUM_LENGTH']))
 		{
+			//Récupère la longueur maximale.
 			$maxLen = $value['CHARACTER_MAXIMUM_LENGTH'];
 
+			//Si la donnée est plus longue que le maximum ou plus petit que 2.
 			if(strlen($input) > $maxLen || strlen($input) < 2)
 			{
+				//Message d'erreur de validation pour ce champ du formulaire.
 				$messagesErreur[$colName] = "Le champs dois être entre 2 et $maxLen caractères.";
 				$estValide = false;
 			}
 		}
+		//Récupère le type de donnée
 		$type = $value['DATA_TYPE'];
 
+		//Si le type est float ou int.
+		//Limitation, car beaucoup plus de type numérique dans MySql, mais ce sont les types utilisés
+		//dans les champs de notre BD pour LogiKek.
 		if ($type == 'float' || $type == 'int')
 		{
+			//Vérifie que la donnée est numérique.
 			if (is_numeric($input))
 			{
+				//Switch selon le type pour utiliser des règles de validation différente.
 				switch($type)
 				{
 					case 'int':
+						//Si ne match pas la regex: Commence et fini par un caractère de chiffre, 1 caractères à plusieurs.
+						// ie: 10
 						if(!preg_match( "/^[0-9]{1,}$/", $input ))
 						{
+							//Déjà confirmé que la donnée est numérique, mais si la regex échoue il est sans doute négatif ou avec des décimals.
+							//Message d'erreur de validation pour ce champ.
 							$messagesErreur[$colName] = "Le champs dois être un entier positif.";
 							$estValide = false;
 						}
 						break;
 						
 					case 'float':
+						//Si ne match pas la regex: Commence et fini par au moins 1 caractère de chiffre, suivi d'un point ou d'une virgule, puis de 2 caractère de chiffre.
+						// ie: 12.22
 						if(!preg_match( "/^[0-9]+[.,][0-9]{2}$/", $input ))
 						{
+							//Message d'erreur de validation pour ce champ.
 							$messagesErreur[$colName] = "Le champs dois être un réel positif avec deux décimals (10.00).";
 							$estValide = false;
 						}
 						break;
-						
+					//Erreur de type, cette fonction devrait être modifié.
 					default:
 						throw new Exception('Validation de ce type de champs non défini.');
 						break;
 				}
 			}
+			//Si contient un caractère de texte.
 			else
 			{
+				//Message d'erreur de validation pour ce champ.
 				$messagesErreur[$colName] = "Le champs dois être numérique.";
 				$estValide = false;
 			}
@@ -283,13 +355,15 @@ function valideForm($nomChamps, $data)
 	{
 		if ($key == 'categories')
 			continue;
+		//Validation spécial, car un champ numérique contenant 0 est interprété comme un false (vide) pour la fonction empty().
 		if (isset($value) && strlen($value) == 0)
 		{
 			$estValide = false;
+			//Message d'erreur de validation pour ce champ.
 			$messagesErreur[$key] = "Champs vide.";
 		}
 	}
-
+	//Retourne le flag de validation.
 	return $estValide;
 }
 
