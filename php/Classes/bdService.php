0c0484c0df00460fba3adb0bca50e7dcd3619884
Tristan <Tri125@users.noreply.github.com>
2016-05-05 08:48:54 -0400

    Update bdService.php

--- php/Classes/bdService.php
+++ php/Classes/bdService.php
@@ -15,18 +15,15 @@ class bdService
 		$config = parse_ini_file(ROOT_DIR.'/config.ini');
 		//Crée un objet mysqli selon les paramètres du fichier de config.
 		$this->BDInterne = new mysqli($config['ip'], $config['username'], $config['password'], $config['dbname']);
-		if (!mysqli_connect_errno())
+		if (mysqli_connect_errno())
 		{
-			//echo "connexion OK <br>";
-		}
-		else
-		{
-			throw(new Exception("Échec de connexion."));
+			throw new Exception("Échec de connexion." . $this->$BDInterne->mysqli_connect_error());	
 		}
 		//Spécifie le charset pour ne pas avoir de problème si Apache est configuré différament sous un autre système.
-		if (!$this->BDInterne->set_charset("utf8")) {
-    		printf("Error loading character set utf8: %s\n", $this->BDInterne->error);
-    	}
+		if (!$this->BDInterne->set_charset("utf8")) 
+		{
+    			printf("Error loading character set utf8: %s\n", $this->BDInterne->error);
+    		}
 	}
 
 	//-----------------------------
